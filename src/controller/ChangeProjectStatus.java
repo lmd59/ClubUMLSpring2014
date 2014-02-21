9cdac8fa598c9b2c500dd79f700f8ea34df91205
XiangCheng <cheng.x@husky.neu.edu>
2014-02-20 23:48:27 -0500

    Defect_18369_EnableProjtoHome

--- src/controller/ChangeProjectStatus.java
+++ src/controller/ChangeProjectStatus.java
@@ -71,7 +71,7 @@ private void processRequest(HttpServletRequest request,
 		ProjectDAO.disableProject(projectId);
 }
 	
-	RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/JSP/home.jsp");
+	RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/JSP/manageProject.jsp");
     dispatcher.forward(request, response);
 	
 }
