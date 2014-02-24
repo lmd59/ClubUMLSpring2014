7fb2ff312d4b4d6431e33d7c538efe03eafa9982
Xiang Cheng <carlcx52a@gmail.com>
2014-02-24 00:57:16 -0500

    Defect_15368_UpdateProj500Err

--- src/controller/ManageProject.java
+++ src/controller/ManageProject.java
@@ -11,6 +11,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import domain.Project;
 import domain.User;
@@ -48,6 +49,11 @@ public class ManageProject extends HttpServlet {
 	
 	
 	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException 
+	{
+			doGet(request,response);	
+	}
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 	//Renders manageProject.jsp
@@ -79,9 +85,10 @@ public class ManageProject extends HttpServlet {
 			}else{
 				inactiveProjects.add(p);
 			}
-			request.setAttribute("inactiveusers", inactiveUsers);
-			request.setAttribute("activeprojects", activeProjects);
-			request.setAttribute("inactiveprojects", inactiveProjects);
+			HttpSession session =request.getSession(true);
+			session.setAttribute("inactiveusers", inactiveUsers);			
+			session.setAttribute("activeprojects", activeProjects);
+			session.setAttribute("inactiveprojects", inactiveProjects);
 		}
 	} catch (SQLException e) {
 		// TODO Auto-generated catch block
